4d0a11b7b4f28fbc508de7652c72dcb9a9b13ed6
crodeheaver <colbyrodeheaver@hotmail.com>
2015-09-04 04:31:52 +0000

    added regex version of peterpiper
    I didn't originally realize there was extra credit, but I'm glad there was. It was fun.

--- peter-piper.js
+++ peter-piper.js
@@ -21,8 +21,6 @@ var counter = 0;
 
 // TODO: Count the number of times an uppercase P or lowercase p character
 //       occurs in the `peterpiper` string.
-for(var i  = 0; i < peterpiper.length; i++)
-    if(peterpiper[i] == "p" || peterpiper[i] == "P")
-        counter++;
+counter = peterpiper.match(/p/gi).length;
 
 console.log("There are " + counter + " P's in Peter Piper.");
